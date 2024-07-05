e88d8fab55aa8acc11f617ccfeaad34f064e54b0
Oussema Louizi <oussemalouizi@gmail.com>
2024-07-05 19:46:59 +0100

    v1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const dotenv=require ("dotenv")
+const mongoose=require("mongoose")
+const app=express()
+const categorieRouter=require("./routes/categorie.route")
+const scategorieRouter=require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
+//middleware
+app.use(express.json())
+dotenv.config()
+
+app.get("/",(req,res)=>{
+    res.send("bienvenue dans notre site")
+})
+
+// Connexion à la base données
+mongoose.connect(process.env.DATABASECLOUD)
+.then(() => {console.log("DataBase Successfully Connected");})
+.catch(err => { console.log("Unable to connect to database", err);
+process.exit(); });
+app.use("/api/categories",categorieRouter)
+app.use("/api/scategories",scategorieRouter)
+app.use('/api/articles', articleRouter)
+
+app.listen(process.env.PORT)
+console.log("application run at port "+process.env.PORT)
